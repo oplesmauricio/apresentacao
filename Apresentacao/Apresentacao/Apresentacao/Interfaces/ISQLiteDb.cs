6e02a7c83abc3acfa1666520cf83abafd86de3f2
Mauricio Ribeiro <mauricioribeiro@MacBook-Pro-de-Mauricio.local>
2019-03-30 11:49:41 -0300

    Branch Inicial Commit

---
+++ Apresentacao/Apresentacao/Apresentacao/Interfaces/ISQLiteDb.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SQLite;
+
+namespace Apresentacao.Interfaces
+{
+    public interface ISQLiteDb
+    {
+        //SQLiteAsyncConnection GetConnection();
+        SQLiteAsyncConnection
+    }
+}
---
+++ Apresentacao/Apresentacao/Apresentacao/Views/ContactsPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Apresentacao.Models;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Apresentacao.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsPage : ContentPage
+    {
+        private ObservableCollection<Contact> _contacts;
+
+        public ContactsPage()
+        {
+            InitializeComponent();
+
+            _contacts = new ObservableCollection<Contact>();
+
+            if (Application.Current.Properties.ContainsKey("1"))
+            {
+                _contacts.Add((Contact)Application.Current.Properties["1"]);
+            }
+            else
+            {
+                _contacts.Add(new Contact { Id = 1, FirstName = "John", LastName = "Smith", Email = "john@smith.com", Phone = "1111" });
+            }
+
+            if (Application.Current.Properties.ContainsKey("2"))
+            {
+                _contacts.Add((Contact)Application.Current.Properties["2"]);
+            }
+            else
+            {
+                _contacts.Add(new Contact { Id = 2, FirstName = "Mary", LastName = "Johnson", Email = "mary@johnson.com", Phone = "2222" });
+            }  
+
+            lvContacts.ItemsSource = _contacts;
+        }
+
+        async void OnAddContact(object sender, System.EventArgs e)
+        {
+            var page = new ContactDetailPage(new Contact());
+
+            // We can subscribe to the ContactAdded event using a lambda expression.
+            // If you're not familiar with this syntax, watch my C# Advanced course. 
+            page.ContactAdded += (source, contact) =>
+            {
+                // ContactAdded event is raised when the user taps the Done button.
+                // Here, we get notified and add this contact to our 
+                // ObservableCollection.
+                _contacts.Add(contact);
+            };
+
+            await Navigation.PushAsync(page);
+        }
+
+        async void OnContactSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            // We need to check if SelectedItem is null because further below 
+            // we de-select the selected item. This will raise another ItemSelected
+            // event, so this method will be called straight away. If we don't
+            // check for null here, we'll get a NullReferenceException.
+            if (lvContacts.SelectedItem == null)
+                return;
+
+            var selectedContact = e.SelectedItem as Contact;
+
+            // We de-select the selected item, so when we come back to the Contacts
+            // page we can tap it again and navigate to ContactDetail. 
+            lvContacts.SelectedItem = null;
+
+            var page = new ContactDetailPage(selectedContact);
+            page.ContactUpdated += (source, contact) =>
+            {
+                // When the target page raises ContactUpdated event, we get 
+                // notified and update properties of the selected contact. 
+                // Here we are dealing with a small class with only a few 
+                // properties. If working with a larger class, you may want 
+                // to look at AutoMapper, which is a convention-based mapping
+                // tool. 
+                selectedContact.Id = contact.Id;
+                selectedContact.FirstName = contact.FirstName;
+                selectedContact.LastName = contact.LastName;
+                selectedContact.Phone = contact.Phone;
+                selectedContact.Email = contact.Email;
+                selectedContact.IsBlocked = contact.IsBlocked;
+            };
+
+            await Navigation.PushAsync(page);
+        }
+
+        async void OnDeleteContact(object sender, System.EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+
+            if (await DisplayAlert("Warning", $"Are you sure you want to delete {contact.FullName}?", "Yes", "No"))
+                _contacts.Remove(contact);
+        }
+    }
+
+}
